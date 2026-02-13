be81fcb05f45874c468afdfc97533e760fc97bb3
thiagolinoz <thiagolinoz@gmail.com>
2026-02-13 20:43:15 -0300

    ajustes nao comitados

--- src/main/java/br/com/fiap/videoapp/domain/ports/out/FileEventPublisherPort.java
+++ src/main/java/br/com/fiap/videoapp/domain/ports/out/FileEventPublisherPort.java
@@ -1,6 +1,5 @@
 package br.com.fiap.videoapp.domain.ports.out;
 
-import br.com.fiap.videoapp.domain.models.VideoModel;
 import br.com.fiap.videoapp.domain.models.events.VideoUploadedModel;
 
 public interface FileEventPublisherPort {
