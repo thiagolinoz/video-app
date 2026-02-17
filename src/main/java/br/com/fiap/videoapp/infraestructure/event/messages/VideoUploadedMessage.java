95bcf5198f9e7570e1dde96af12db81cfb51bdb9
maycon-alves <maycon_valdemiro@hotmail.com>
2026-02-17 19:28:27 -0300

    adicionando formatacao as datas do kafka

--- src/main/java/br/com/fiap/videoapp/infraestructure/event/messages/VideoUploadedMessage.java
+++ src/main/java/br/com/fiap/videoapp/infraestructure/event/messages/VideoUploadedMessage.java
@@ -1,5 +1,7 @@
 package br.com.fiap.videoapp.infraestructure.event.messages;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public record VideoUploadedMessage(
@@ -9,7 +11,9 @@ public record VideoUploadedMessage(
         String nmVideo,
         String nmVideoPathOrigin,
         String nmVideoPathZip,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         Date dateTimeVideoCreated,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         Date dateTimeVideoProcessCompleted,
         String nmPersonName
 ) {
